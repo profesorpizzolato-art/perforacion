cf79965d57adbef94b9898a806d94526211b0ec4
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-05-05 20:02:48 -0300

    Create certificado.py

---
+++ reports/certificado.py
@@ -0,0 +1,9 @@
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+
+def generar_certificado(nombre):
+    doc = SimpleDocTemplate(f"{nombre}_certificado.pdf")
+
+    content = []
+    content.append(Paragraph(f"Certificado MENFA para {nombre}", None))
+
+    doc.build(content)
