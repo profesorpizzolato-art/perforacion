c2f47ba4ac1d057619ec45f92b5486ea5ede9f56
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-05-05 20:11:33 -0300

    Update engine.py

--- sim_engine/engine.py
+++ sim_engine/engine.py
@@ -3,7 +3,10 @@ from sim_engine.perforacion.hidraulica import calcular_hhp
 from sim_engine.torque_drag.torque import calcular_torque
 from sim_engine.geonavegacion.trayectoria import calcular_trayectoria
 from sim_engine.eventos.event_manager import evaluar_eventos
+from sim_engine.control_pozos.kick import detectar_kick
 
+if detectar_kick(state):
+    resultado["alerta"] = "KICK DETECTADO"
 def run_simulation(state):
     resultado = {}
 
