9d4c39c3bb1e08e13c4967f055b86cce8c091ba6
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-05-05 19:53:57 -0300

    Create security.py

---
+++ backend/auth/security.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+
+SECRET = "MENFA_SECRET"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def verify_password(password, hashed):
+    return pwd_context.verify(password, hashed)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    to_encode["exp"] = datetime.utcnow() + timedelta(hours=8)
+    return jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
