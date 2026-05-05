091737d5155b44aa97056ecdaa8c6d228af12db9
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-05-05 20:07:57 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,9 @@ from backend.routes import simulacion
 app = FastAPI(title="MENFA API")
 
 app.include_router(simulacion.router)
+from fastapi import WebSocket
+from backend.realtime.ws import connect
+
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await connect(ws)
